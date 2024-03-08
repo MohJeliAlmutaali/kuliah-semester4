928e75b6c93331a2dd984421c699ac965d35434b
MohJeliAlmutaali <114728937+MohJeliAlmutaali@users.noreply.github.com>
2024-03-08 20:13:16 +0700

    sesi 4

---
+++ OOP/sesi4/SameSide.java
@@ -0,0 +1,26 @@
+public class SameSide extends Triangle {
+
+    private Double side;
+
+    public SameSide(String name, Double side) {
+        super();
+        this.name = name;
+        this.side = side;
+        this.calculateArea();
+        super.side1 = side;
+        super.side2 = side;
+        super.side3 = side;
+    }
+
+    @Override
+    public void calculateArea() {
+        super.area = (Math.sqrt(3) / 4) * side * side;
+    }
+
+    @Override
+    public void getInfo() {
+        System.out.println("\nSameSide " + name + " info");
+        System.out.println("Area: " + super.area);
+        System.out.println("Side: " + side);
+    }
+}
